2dfe7e100f7586942c6582d09e9a2387a85d1402
KrisKip123 <kriskip12393@gmail.com>
2023-02-22 16:59:22 +0300

    feat: add Home Page

--- front-app/src/pages/HomePage/HomePage.tsx
+++ front-app/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,33 @@
+import Container from "@mui/material/Container/Container";
+
+import { motion } from "framer-motion";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container
+      component={motion.section}
+      initial={{
+        y: "-120%",
+      }}
+      animate={{
+        y: 0,
+      }}
+      exit={{
+        y: "120%",
+      }}
+      transition={{
+        duration: 0.8,
+        ease: "easeIn",
+      }}
+      maxWidth={false}
+      disableGutters
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>KRISKIP123</div>
+    </Container>
+  );
 };
